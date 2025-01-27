d8a0e9122280a2f1fb3980d9ad4af2438ecb5b32
LedioKoci03 <127228600+LedioKoci03@users.noreply.github.com>
2025-01-27 21:53:25 +0100

    user class added

---
+++ src/main/java/model/User.java
@@ -0,0 +1,26 @@
+package model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "\"user\"")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+}
